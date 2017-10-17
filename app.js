9c2cf7aa92359f93f5268b0492b0a2ad46d46ce6
thanhan7914 <thanhan7914@gmail.com>
2017-10-17 11:34:16 +0700

    ejs

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(session({
 }));
 
 //set views engine
-app.use('/public',express.static( __dirname + '/public'));
-app.set('views', __dirname + '/views');
+app.use('/public', express.static( __dirname + '/public'));
+app.set('views', __dirname + 'views');
 app.set('view engine','ejs');
 
 app.use(require('./midez'));
