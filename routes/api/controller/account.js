dbf6ea06bef25668c809f2056ebd28577b42d8ee
thanhan7914 <thanhan7914@gmail.com>
2017-11-26 16:45:30 +0700

    - modify account_controller

--- routes/api/controller/account.js
+++ routes/api/controller/account.js
@@ -3,6 +3,7 @@ const Account = require('../../../model/account');
 
 exports.get_list = function(req, res) {
     Account.find({})
+    .select('username')
     .then(res.array_dump)
     .catch(res.handle_error);
 };
@@ -10,6 +11,8 @@ exports.get_list = function(req, res) {
 exports.add_account = function(req, res) {
     if(!_.isString(req.body.username) || !_.isString(req.body.password))
        return res.handle_error(new Error('missing parameter'));
+    if(req.body.username.trim() === '' || req.body.password.trim() === '')
+       return res.handle_error(new Error('username or password not empty'));
 
     Account.create({username: req.body.username, password: req.body.password})
     .then((account) => {
